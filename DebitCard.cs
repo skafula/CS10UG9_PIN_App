92e6a013ccc65439c18bb44b60013c3f5c474a70
skafula <79100340+skafula@users.noreply.github.com>
2023-07-16 14:42:11 +0200

    Add files via upload

---
+++ DebitCard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMPin_App
+{
+    internal class DebitCard
+    {
+        private string _pinCode;
+
+        public string PinCode
+        {
+            set 
+            {
+                if (IsValidPin(value))
+                {
+                    _pinCode = value;
+                }
+            }
+            get { return _pinCode; }
+        }
+
+        private bool IsValidPin(string pinCode)
+        {
+            bool isValid = int.TryParse(pinCode, out int number);
+            if (isValid && (pinCode.Length == 4 || pinCode.Length == 6))
+            {
+                Console.WriteLine("Your PIN code successfully created!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("PIN code must be 4 or 6 digits and could contain only numbers!");
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using ATMPin_App;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DebitCard checkPin = new DebitCard();
+
+        Console.WriteLine("Welcome, create your PIN code. It must be 4 or 6 numbers: ");
+
+        do
+        {
+            checkPin.PinCode = Console.ReadLine();
+        } while (checkPin.PinCode == null);
+
+        
+
+        Console.WriteLine("value in _pinCode field: " + checkPin.PinCode);
+
+
+    }
+}
